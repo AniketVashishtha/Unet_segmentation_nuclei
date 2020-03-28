8ca17d09d244b447b0a18482d80339bc9ae0c3fb
AniketVashishtha <53573925+AniketVashishtha@users.noreply.github.com>
2020-03-28 19:11:35 +0530

    Add files via upload

---
+++ Unet_nuclei.py
@@ -0,0 +1,202 @@
+from tensorflow_core import *
+import tensorflow as tf
+import os
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from skimage.io import imread,imshow
+from skimage.transfrom import resize
+import matplotlib.pyplot as plt
+import random
+
+TRAIN_PATH = '/stage1_train'
+train_ids = next(os.walk(TRAIN_PATH))[1]
+
+
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+
+
+X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+
+print('Resizing training images and masks')
+for n,id_ in tqdm(enumerate(train_ids), total = len(train_ids)):
+    path = TRAIN_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+     img = resize(img,(IMG_HEIGHT, IMG_WIDTH), mode = 'constant', preserve_ange = True)
+     X_train[n] = img #fill empty xtrai with values from img
+     mask = np.zeros((IMG_HEIGHT,IMG_WIDTH,1),dtype = np.bool)
+     for mask_file in next(os.walk(path + '/masks/'))[2]:
+         mask_ = imread(path + '/masks/'+ mask_file)
+         mask_ = np.expand_dims(resize(mask_,(IMG HEIGHT, IMG WIDTH) mode = 'constant',preserve_range = True),axis= -1)
+         mask = np.maximum(mask,mask_)
+
+    Y_train[n] = mask
+    
+#test images
+X_test = np.zeroes((len(test_ids), IMG_HEIGHT, IMG_WIDTH,IMG_CHANNELS),dtype = np.uint8)
+sizes_text = []
+print('Resizing test images')
+for n,id_ in tqdm(enumerate(test_ids),total = len(test_ids)):
+    path = TEST_PATH + id_
+    img = imread(path + '/images/' + id_ + '.png' )[:,:,:IMG_CHANNELS]
+    sizes_test.append([img.shape[0],img.shape[1]])
+    img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode = 'constant', preserve_range = True)
+    X_test[n] = img
+    
+
+
+image_x = random.randint(0,len(train_ids))
+imshow(X_train[image_x])
+plt.show()
+imshow(np.squeeze(Y_train[image_x]))
+plt.show()
+
+inputs = tf.keras.layers.Input((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
+
+#Contraction of U path
+s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
+
+c1 = tf.keras.layers.Conv2D(16, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(s)
+c1 = tf.keras.layers.Dropout(0.1)(c1)
+c1 = tf.keras.layers.Conv2D(16, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c1)
+p1 = tf.keras.layers.MaxPooling2D((2,2))(c1)
+
+c2 = tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(p1)
+c2 = tf.keras.layers.Dropout(0.1)(c2)
+c2 = tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c2)
+p2 = tf.keras.layers.MaxPooling2D((2,2))(c2)
+
+c3 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(p2)
+c3 = tf.keras.layers.Dropout(0.1)(c3)
+c3 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c3)
+p3 = tf.keras.layers.MaxPooling2D((2,2))(c3)
+
+c4 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(p3)
+c4 = tf.keras.layers.Dropout(0.1)(c4)
+c4 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c4)
+p4 = tf.keras.layers.MaxPooling2D((2,2))(c4)
+
+c5 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(p4)
+c5 = tf.keras.layers.Dropout(0.1)(c5)
+c5 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c5)
+
+#Expansion of U path
+u6 = tf.keras.layers.Conv2DTranspose(128,(2,2), strides = (2,2), padding = 'same')(c5)
+u6 = tf.keras.layers.concatenate([u6,c4])
+c6 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u6)
+c6 = tf.keras.layers.Dropout(0.1)(c6)
+c6 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c6)
+
+u7 = tf.keras.layers.Conv2DTranspose(64,(2,2), strides = (2,2), padding = 'same')(c6)
+u7 = tf.keras.layers.concatenate([u7,c3])
+c7 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u7)
+c7 = tf.keras.layers.Dropout(0.1)(c7)
+c7 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c7)
+
+u8 = tf.keras.layers.Conv2DTranspose(32,(2,2), strides = (2,2), padding = 'same')(c7)
+u8 = tf.keras.layers.concatenate([u8,c2])
+c8 = tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u8)
+c8 = tf.keras.layers.Dropout(0.1)(c8)
+c8 = tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c8)
+
+u9 = tf.keras.layers.Conv2DTranspose(16,(2,2) ,strides = (2,2), padding = 'same')(c8)
+u9 = tf.keras.layers.concatenate([u9,c1])
+c9 = tf.keras.layers.Conv2D(16, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(u9)
+c9 = tf.keras.layers.Dropout(0.1)(c9)
+c9 = tf.keras.layers.Conv2D(16, (3,3), activation = 'relu', kernel_initializer = 'he_normal', padding = 'same')(c9)
+
+outputs = tf.keras.layers.Conv2D(1,(1,1), activation = 'sigmoid')(c9) 
+
+model = tf.keras.Model(inputs = [inputs], outputs = [outputs])
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.summary()
+
+#Checkpoint
+checkpointer = tf.keras.callbacks.ModelCheckpoint('../input/lgg-mri-segmentation/kaggle_3m',verbose = 1, save_best_only = True)
+callbacks = [tf.keras.callbacks.EarlyStopping(patience = 2, monitor='val_loss'),
+             tf.keras.callbacks.TensorBoard(log_dir = 'logs')]
+results = model.fit(X,Y, validation_split = 0.1,batch_size = 16, epochs = 25, callbacks = callbacks)
+
+# Check if training data looks all right
+ix = random.randint(0, len(train_ids))
+imshow(X_train[ix])
+plt.show()
+imshow(np.squeeze(Y_train[ix]))
+plt.show()
+
+# Define IoU metric
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+# Predict on train, val and test
+model = load_model('model-dsbowl2018-1.h5', custom_objects={'mean_iou': mean_iou})
+preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+preds_test = model.predict(X_test, verbose=1)
+
+# Threshold predictions
+preds_train_t = (preds_train > 0.5).astype(np.uint8)
+preds_val_t = (preds_val > 0.5).astype(np.uint8)
+preds_test_t = (preds_test > 0.5).astype(np.uint8)
+
+# Create list of upsampled test masks
+preds_test_upsampled = []
+for i in range(len(preds_test)):
+    preds_test_upsampled.append(resize(np.squeeze(preds_test[i]), 
+                                       (sizes_test[i][0], sizes_test[i][1]), 
+                                       mode='constant', preserve_range=True))
+    
+# Perform a sanity check on some random training samples
+ix = random.randint(0, len(preds_train_t))
+imshow(X_train[ix])
+plt.show()
+imshow(np.squeeze(Y_train[ix]))
+plt.show()
+imshow(np.squeeze(preds_train_t[ix]))
+plt.show()
+
+# Perform a sanity check on some random validation samples
+ix = random.randint(0, len(preds_val_t))
+imshow(X_train[int(X_train.shape[0]*0.9):][ix])
+plt.show()
+imshow(np.squeeze(Y_train[int(Y_train.shape[0]*0.9):][ix]))
+plt.show()
+imshow(np.squeeze(preds_val_t[ix]))
+plt.show()
+
+# Run-length encoding stolen from https://www.kaggle.com/rakhlin/fast-run-length-encoding-python
+def rle_encoding(x):
+    dots = np.where(x.T.flatten() == 1)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+
+def prob_to_rles(x, cutoff=0.5):
+    lab_img = label(x > cutoff)
+    for i in range(1, lab_img.max() + 1):
+        yield rle_encoding(lab_img == i)
+
+
+new_test_ids = []
+rles = []
+for n, id_ in enumerate(test_ids):
+    rle = list(prob_to_rles(preds_test_upsampled[n]))
+    rles.extend(rle)
+    new_test_ids.extend([id_] * len(rle))
+    
+    
